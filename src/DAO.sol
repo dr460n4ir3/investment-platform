81124a58c425969dc7c6894daaefaef141c6b964
dr460n4ir3 <token.b@gmail.com>
2024-06-20 04:02:48 -0400

    Add DAO, RewardNFT, and GovernanceToken contracts for decentralized governance and reward system

--- src/DAO.sol
+++ src/DAO.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "lib/openzeppelin/access/Ownable.sol";
+import "lib/openzeppelin/token/ERC20/IERC20.sol";
 import "./RewardNFT.sol";
 
 contract DAO is Ownable {
--- src/RewardNFT.sol
+++ src/RewardNFT.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "lib/openzeppelin/token/ERC721/ERC721.sol";
+import "lib/openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
+import "lib/openzeppelin/access/Ownable.sol";
 
 contract RewardNFT is ERC721URIStorage, Ownable {
     uint256 public tokenCounter;
 
-    constructor() ERC721("Dr460nNFT", "dNFT") {
+    constructor() ERC721("Reward NFT", "RNFT") {
         tokenCounter = 0;
     }
 
